653728b3c303b7ee3e91e58647b84fef47c88635
Dasaribobby <145694588+Dasaribobby@users.noreply.github.com>
2026-06-27 18:08:57 +0530

    server 500 request

--- shopease/settings.py
+++ shopease/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = 'shopease.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+# Use DATABASE_URL if available (for production on Render), otherwise use SQLite
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
